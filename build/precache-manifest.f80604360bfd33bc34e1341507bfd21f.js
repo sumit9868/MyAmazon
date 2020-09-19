b38e20dfbd250965e99b12e93f6a8b92649327e5
varunjeett <abhijeetvarun@gmail.com>
2020-09-19 13:55:22 +0530

    fixed login seperator

--- build/precache-manifest.f80604360bfd33bc34e1341507bfd21f.js
+++ build/precache-manifest.f80604360bfd33bc34e1341507bfd21f.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4f252881d2b14de45a9e0788472b7f51",
+    "revision": "5039d5ed068b99fb1721b8a9f4092dd4",
     "url": "/index.html"
   },
   {
-    "revision": "3334e1f75510e227f147",
-    "url": "/static/css/main.b86aab05.chunk.css"
+    "revision": "7d821bf6fefa1fb11fd3",
+    "url": "/static/css/main.8bb05105.chunk.css"
   },
   {
     "revision": "e82056ff864f8ef40595",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.071f7fa3.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3334e1f75510e227f147",
-    "url": "/static/js/main.83825ca5.chunk.js"
+    "revision": "7d821bf6fefa1fb11fd3",
+    "url": "/static/js/main.3e9c932a.chunk.js"
   },
   {
     "revision": "5aaebd4fe95b6cfde2ef",
--- src/Login.js
+++ src/Login.js
@@ -1,81 +1,88 @@
-import React, { useState } from 'react'
-import './Login.css'
+import React, { useState } from "react";
+import "./Login.css";
 import { Link, useHistory } from "react-router-dom";
-import { auth } from './firebase';
+import { auth } from "./firebase";
 
 function Login() {
+  const history = useHistory();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const history = useHistory();
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const login = (event) => {
+    //prevents refresh
+    event.preventDefault();
 
-    const login = (event) => {
-        //prevents refresh
-        event.preventDefault();
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then((auth) => {
+        //logged in , go to personal home
+        history.push("/");
+      })
 
-        auth.signInWithEmailAndPassword(email, password)
-            .then((auth) => {
-                //logged in , go to personal home
-                history.push("/");
-            })
+      .catch((e) => alert(e.message));
+  };
 
-            .catch((e) => alert(e.message));
-    }
+  const register = (event) => {
+    //prevents refresh
+    event.preventDefault();
 
-    const register = (event) => {
-        //prevents refresh
-        event.preventDefault();
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((auth) => {
+        //user created
+        history.push("/");
+      })
+      .catch((e) => alert(e.message));
+  };
 
-        auth.createUserWithEmailAndPassword(email, password)
-            .then((auth) => {
-                //user created
-                history.push("/");
-            })
-            .catch((e) => alert(e.message));
-    }
+  return (
+    <div className="login">
+      <Link to="/">
+        <img
+          className="login__logo"
+          src="https://www.marketplace.org/wp-content/uploads/2019/07/ama2.png"
+          alt="amazon_logo"
+        />
+      </Link>
 
-    return (
-        <div className="login">
+      <div className="login__container">
+        <h1>Sign in</h1>
+        <form>
+          <h5>E-mail</h5>
+          <input
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            placeholder="Enter your E-mail"
+            type="email"
+          />
+          <h5>Password</h5>
+          <input
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            placeholder="Enter your Password"
+            type="password"
+          />
 
-            <Link to="/">
-                <img className="login__logo"
-                    src="https://www.marketplace.org/wp-content/uploads/2019/07/ama2.png"
-                    alt="amazon_logo"
-                />
-            </Link>
+          <button onClick={login} type="submit" className="signin__button">
+            Sign In
+          </button>
+        </form>
 
-            <div className="login__container">
-                <h1>Sign in</h1>
-                <form>
-                    <h5>E-mail</h5>
-                    <input value={email} onChange={event => setEmail(event.target.value)} placeholder="Enter your E-mail" type="email" />
-                    <h5>Password</h5>
-                    <input value={password} onChange={event => setPassword(event.target.value)} placeholder="Enter your Password" type="password" />
+        <p>
+          By continuing, you agree to Amazon's Conditions of Use and Privacy
+          Notice.
+        </p>
 
-                    <button onClick={login} type="submit" className="signin__button">Sign In</button>
-                </form>
+        <div class="separator">New to Amazon ?</div>
 
-                <p>
-                    By continuing, you agree to Amazon's Conditions of Use and Privacy Notice.
-                </p>
+        <button onClick={register} className="signup__button">
+          Create your Amazon Account
+        </button>
+      </div>
 
-
-                <div className="seperator" >
-                    <hr></hr>
-                    <div className="seperator__div" >
-                        <h5 className="seperator__title" >New to Amazon?</h5>
-                    </div>
-                </div>
-                <button onClick={register} className="signup__button">Create your Amazon Account</button>
-
-            </div>
-
-            {/* <h1>Login Page</h1> */}
-        </div>
-    );
+      {/* <h1>Login Page</h1> */}
+    </div>
+  );
 }
 
-export default Login
-
-
-{/* By continuing, you agree to Amazon's Conditions of Use and Privacy Notice. */ }
+export default Login;
